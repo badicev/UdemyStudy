5117af954910a11cd773989888eeea8645fd5d9d
badicev <basakdilaracevik@hotmail.com>
2022-12-05 23:49:58 +0300

    Snake game start

---
+++ MyWork/Day 20/snake game.py
@@ -0,0 +1,15 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("gray")
+screen.title(("Snek Oyunu"))
+
+
+
+
+
+
+
+
+screen.exitonclick()
