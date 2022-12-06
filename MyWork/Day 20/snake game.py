7e31937b2eb3d18d0c690dd73fec31d726540fc7
badicev <basakdilaracevik@hotmail.com>
2022-12-06 19:33:45 +0300

    Snake game Part 2

--- MyWork/Day 20/snake game.py
+++ MyWork/Day 20/snake game.py
@@ -1,6 +1,8 @@
 import turtle
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -10,13 +12,14 @@ screen.title("Snek Oyunu")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-
+screen.onkeypress(snake.up, "Up")
+screen.onkeypress(snake.down, "Down")
+screen.onkeypress(snake.left, "Left")
+screen.onkeypress(snake.right, "Right")
 
 screen.update()
 
@@ -25,12 +28,33 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # Detect collision with food.
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    # Detect collision with wall.
+    if snake.segments[0].xcor() > 300 or snake.segments[0].xcor() < -300 or snake.segments[0].ycor() > 300 or \
+            snake.segments[0].ycor() < -300:
+        game_is_on = False
+        scoreboard.game_over()
 
+    # Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.segments[0]:
+            pass
+        elif snake.segments[0].distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
+    # for segment in snake.segments[1:]:
+    #     if snake.segments[0].distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
 
+    # If head collides with any segment in the tail: trigger game_over
 
 screen.exitonclick()
--- MyWork/Day 20/snake.py
+++ MyWork/Day 20/snake.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
 
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)] #constant
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]  # constant
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
-RIGHT = 90
+RIGHT = 0
+
 
 class Snake:
 
@@ -15,11 +16,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("green")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("green")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -29,17 +36,15 @@ class Snake:
         self.segments[0].forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.segments[0].heading() != DOWN:
-            self.segments[0].setheading(UP)
-
-    def down(self):
-        if self.segments[0].heading() != UP:
-            self.segments[0].setheading(DOWN)
+        self.segments[0].setheading(UP)
 
     def right(self):
         if self.segments[0].heading() != LEFT:
             self.segments[0].setheading(RIGHT)
 
+    def down(self):
+        self.segments[0].setheading(DOWN)
+
     def left(self):
-        if self.segments[0].heading() != RIGHT:
-            self.segments[0].setheading(LEFT)
+        # if self.segments[0].heading() != RIGHT:
+        self.segments[0].setheading(LEFT)
