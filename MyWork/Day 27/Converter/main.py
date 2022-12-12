7ef1a28a2054fdab29f882027bdec88afa0c0de0
badicev <basakdilaracevik@hotmail.com>
2022-12-12 17:27:49 +0300

    WGS84 to Web Mercator Converter

---
+++ MyWork/Day 27/Converter/main.py
@@ -0,0 +1,58 @@
+"""World Geodetic System (WGS 84) to Web Mercator"""
+import math
+# x: Web Mercator coordinate
+# y: Web Mercator coordinate
+# longitude: WGS 84 coordinate
+# latitude: WGS 84 coordinate
+
+
+from tkinter import *
+from math import pi, log, tan
+
+
+def wgs84_to_webmercator():
+    wgs84_x = float(wgs84_input_x.get())
+    wgs84_y = float(wgs84_input_y.get())
+
+    longitude = wgs84_y * (math.pi / 180)
+    latitude = wgs84_x * (math.pi / 180)
+
+    mercator_result_x = longitude * (20037508.34 / 180)
+    mercator_result_y = math.log(tan((90 + latitude) * math.pi / 360)) / (math.pi / 180)
+    mercator_result_y = mercator_result_y * (20037508.34 / 180)
+
+    mercator_result_label_x.config(text=f"{mercator_result_x}")
+    mercator_result_label_y.config(text=f"{mercator_result_y}")
+
+# Wrong results :D
+
+window = Tk()
+window.title("WGS 84 to Web Mercator converter")
+window.config(padx=20, pady=20)
+
+wgs84_input_x = Entry(width=10)
+wgs84_input_x.grid(column=1, row=0)
+
+wgs84_input_y = Entry(width=10)
+wgs84_input_y.grid(column=2, row=0)
+
+wgs84_label = Label(text="WGS 84 Coordinates")
+wgs84_label.grid(column=3, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+mercator_result_label_x = Label(text="0")
+mercator_result_label_x.grid(column=1, row=1)
+
+mercator_result_label_y = Label(text="0")
+mercator_result_label_y.grid(column=2, row=1)
+
+mercator_label = Label(text="Web Mercator")
+mercator_label.grid(column=3, row=1)
+
+calculate_button = Button(text="Calculate", command=wgs84_to_webmercator)
+calculate_button.grid(column=1, row=2)
+calculate_button.config(borderwidth=10)
+
+window.mainloop()
--- MyWork/Day 27/main.py
+++ MyWork/Day 27/main.py
@@ -5,11 +5,14 @@ import tkinter
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # label
 my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-#my_label.pack()
-my_label.place(x=0, y=0)
+# my_label.pack()
+# my_label.place(x=0, y=0)
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 my_label["text"] = "New Text"
 # or
@@ -24,19 +27,24 @@ def button_clicked():
 
 
 button = tkinter.Button(text="Click Me!", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+# button.pack()
+
+
+new_button = tkinter.Button(text="I am New!", command=button_clicked)
+new_button.grid(column=2, row=0)
 
 # Entry
 
 input = tkinter.Entry(width=20)
 print(input.get())
-input.pack()
+input.grid(column=3, row=2)
+# input.pack()
 
 # pack: packs widgets next to each other
 # place: places to precise positions
-# grid:
-
-
+# grid: creates a collection of horizontal and vertical lines creating
+# a pattern against which we can line up our design elements
 
 
 '''#######################'''
