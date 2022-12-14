fe868bc2b8ef6599a5cbcf56a5f23a9908b943b3
badicev <basakdilaracevik@hotmail.com>
2022-12-15 00:22:10 +0300

    Flash Card App Part2

--- MyWork/Day 31/main.py
+++ MyWork/Day 31/main.py
@@ -1,3 +1,4 @@
+# http://www.russianforeveryone.com/RufeA/Lessons/Introduction/Alphabet/Alphabet.htm
 # open file with russian alphabet
 import codecs
 import sys
@@ -6,7 +7,22 @@ from tkinter import *
 # with codecs.open('Ru-Alphabet.csv', 'r', encoding='utf-8') as f:
 #     data = f.read()
 #     print(data)
+# ---------------------------CHANGE MECHANISM--------------------------------#
+import pandas as pd
+import random
 
+ru = pd.read_csv("Ru-Alphabet.csv")
+print(ru)
+ru_alphabet_dict = ru.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(ru_alphabet_dict)
+    canvas.itemconfig(up, text="Russian")
+    canvas.itemconfig(down, text=current_card["Russian"])
+
+
+# ---------------------------UI SETUP--------------------------------#
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -19,36 +35,40 @@ FONT_NAME = "Courier"
 
 window = Tk()
 window.title("Flash Card App")
-
 window.config(padx=50, pady=50, bg=LIGHT_GREEN)
 
 canvas = Canvas(width=800, height=526, bg=LIGHT_GREEN, highlightthickness=False)
-canvas.grid(column=1, row=0)
 
 # IMAGE-PATH
 
 import os
 
-
-def resource_path(relative_path):
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
-
-front = PhotoImage(file=resource_path("images/card_front.png"))
-back = PhotoImage(file=resource_path("images/card_back.png"))
-right = PhotoImage(file=resource_path("images/right.png"))
-wrong = PhotoImage(file=resource_path("images/wrong.png"))
-button = Button(image=front, highlightthickness=0)
-canvas.grid(column=1, row=2, columnspan=2)
-canvas.create_image(350, 213, image=front)
+# def resource_path(relative_path):
+#     try:
+#         # PyInstaller creates a temp folder and stores path in _MEIPASS
+#         base_path = sys._MEIPASS
+#     except Exception:
+#         base_path = os.path.abspath(".")
+#
+#     return os.path.join(base_path, relative_path)
 
 
+front = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=front)
+up = canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
+down = canvas.create_text(400, 263, text="word", font=(FONT_NAME, 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
 
+back = PhotoImage(file="images/card_back.png")
+
+right = PhotoImage(file="images/right.png")
+right_button = Button(image=right, highlightthickness=0, command=next_card)
+right_button.grid(row=1, column=0)
+
+wrong = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong, highlightthickness=0, command=next_card)
+wrong_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
