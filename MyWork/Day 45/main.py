55fc9a1ebe1d1030806fc56c436b9089f7d116d0
badicev <basakdilaracevik@hotmail.com>
2022-12-18 13:09:12 +0300

    BeautifulSoup Part2

--- MyWork/Day 45/main.py
+++ MyWork/Day 45/main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 with open("website.html", encoding="utf-8") as file:
     contents = file.read()
 
-
 soup = BeautifulSoup(contents, "html.parser")
 print(soup.title)
 print(soup.title.name)
@@ -19,4 +18,21 @@ all_anchor_tags = soup.find_all(name="a")
 print(all_anchor_tags)
 
 for tag in all_anchor_tags:
-    print(tag.getText())
+    # print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.getText())
+
+bootcamp_url = soup.select_one(selector="p a")  # CSS selector
+print(bootcamp_url)
+
+
+name = soup.select_one(selector="#name")
+print(name)
+
+headings = soup.select(".heading")
+print(headings)
